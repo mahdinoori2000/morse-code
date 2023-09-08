35f3efe44fd9a49e4905df9b71795ca76ba4737a
geekelo <efuelight12@gmail.com>
2023-09-08 09:50:26 +0100

    Fix linter errors

--- decode.rb
+++ decode.rb
@@ -1,28 +1,37 @@
 def decode_char(char)
-  code_morse = { '.-'    => 'A', '-...'  => 'B', '-.-.' => 'C', '-..'   => 'D', '.'     => 'E', '..-.'  => 'F', '--.'   => 'G',
-                 '....'  => 'H', '..'    => 'I', '.---' => 'J', '-.-'   => 'K', '.-..'  => 'L', '--'    => 'M', '-.'    => 'N',
-                 '---'   => 'O', '.--.'  => 'P', '--.-' => 'Q', '.-.'   => 'R', '...'   => 'S', '-'     => 'T', '..-'   => 'U',
-                 '...-'  => 'V', '.--'   => 'W', '-..-' => 'X', '-.--'  => 'Y', '--..'  => 'Z' }
-
+  code_morse = {
+    '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E',
+    '..-.' => 'F', '--.' => 'G', '....' => 'H', '..' => 'I', '.---' => 'J',
+    '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N', '---' => 'O',
+    '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T',
+    '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X', '-.--' => 'Y',
+    '--..' => 'Z'
+  }
   code_morse[char]
 end
-
 def decode_word(word)
-  word = word.split(' ')
+  word = word.split
   decoded_word = ''
   word.each do |letter|
     decoded_word += decode_char(letter)
   end
   decoded_word
 end
-
 def decode(sentence)
   decoded_sentence = ''
   sentence = sentence.split('   ')
   sentence.each do |word|
     decoded_sentence += " #{decode_word(word)}"
   end
-  print decoded_sentence.strip
+  puts decoded_sentence.strip
 end
-
 decode('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
+
+
+
+
+
+
+
+
+
