11fff2b4e072ba6e2e37f0d49877aadfb1f417ac
Mahdi Noori <mahdinoori2389@gmail.com>
2023-09-08 12:38:11 +0430

    Fix linter errors

--- decode.rb
+++ decode.rb
@@ -1,25 +1,29 @@
 $code_morse = { ".-" => "A", "-..." => "B", "-.-." => "C", "-.." => "D", "." => "E", "..-." => "F", "--." => "G",
-                "...." => "H", ".." => "I", ".---" => "J", "-.-" => "K", ".-.." => "L", "--" => "M", "-." => "N", "---" => "O", ".--." => "P", "--.-" => "Q", ".-." => "R", "..." => "S", "-" => "T", "..-" => "U", "...-" => "V", ".--" => "W", "-..-" => "X", "-.--" => "Y", "--.." => "Z" }
+                "...." => "H", ".." => "I", ".---" => "J", "-.-" => "K", ".-.." =>
+                 "L", "--" => "M", "-." => "N", "---" => "O", ".--." =>
+                  "P", "--.-" => "Q", ".-." => "R", "..." => "S", "-" =>
+                   "T", "..-" => "U", "...-" => "V", ".--" => "W", "-..-" =>
+                    "X", "-.--" => "Y", "--.." => "Z" }
 
 def decode_char(char)
   $code_morse[char]
 end
 
 def decode_word(word)
-    word = word.split(" ")
-    decoded_word = ""
-    word.each do |letter|
-        decoded_word += decode_char(letter)
-    end
-    decoded_word
+  word = word.split(" ")
+  decoded_word = ""
+  word.each do |letter|
+    decoded_word += decode_char(letter)
+  end
+  decoded_word
 end
+
 def decode(sentence)
-    decoded_sentence = ""
-    sentence = sentence.split("   ")
-    sentence.each do |word|
-        decoded_sentence += " #{decode_word(word)}"
-    end
-    print decoded_sentence
+  decoded_sentence = ""
+  sentence = sentence.split("   ")
+  sentence.each do |word|
+    decoded_sentence += " #{decode_word(word)}"
+  end
+  print decoded_sentence
 end
 decode(".-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...")
-
