83c2f45479b2697c1e693fa10e483e6ea70ae984
geekelo <efuelight12@gmail.com>
2023-09-08 08:56:44 +0100

    Add functions to handle conversions

--- decode.rb
+++ decode.rb
@@ -1,1 +1,21 @@
 print 'mahdi'
+
+
+def decode_word(word)
+    word = word.split(" ")
+    decoded_word = ""
+    word.each do |letter|
+        decoded_word += decode_char(letter)
+    end
+    decoded_word
+end
+def decode(sentence)
+    decoded_sentence = ""
+    sentence = sentence.split("   ")
+    sentence.each do |word|
+        decoded_sentence += " #{decode_word(word)}"
+    end
+    print decoded_sentence
+end
+decode(".-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...")
+
